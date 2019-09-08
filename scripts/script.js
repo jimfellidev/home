03eb9c25b357f9d17ed1d116d6015450545fc3a2
Jim Felli <jimfellidev@gmail.com>
2019-09-08 07:05:59 -0400

    borders

--- scripts/script.js
+++ scripts/script.js
@@ -6,12 +6,43 @@ const hamburger = document.querySelector(".hamburger");
 const addDropper = document.querySelector(".navbar");
 
 
+
 // HAMBURGALER //
 hamburger.addEventListener("click", function() {
     hamburger.classList.toggle("is-active");
     if (hamburger.classList.contains('is-active')){
       addDropper.classList.add("nav__active");
+      hamburger.classList.add("nav__plus--active");
     } else {
       addDropper.classList.remove("nav__active");
+      hamburger.classList.remove("nav__plus--active");
     } 
   });
+
+//   function isDescendant(parent, child) {
+//     var node = child.parentNode;
+//     while (node != null) {
+//       if (node == parent) {
+//         return true;
+//       }
+//       node = node.parentNode;
+//     }
+//     return false;
+//   }
+  
+// // re-write 
+
+
+//   document.addEventListener("click", function(event){
+//     console.log('event.target', event.target);    
+//     const nav = document.querySelector(".nav-wrapper");
+//     var isInNav = isDescendant(nav, event.target);
+//     console.log('isInNav', isInNav);
+//     if (hamburger.classList.contains('is-active') && isInNav) {
+//       console.log('true')      
+//     } else {
+//       console.log('false')
+//       menuId.classList.remove("show");
+//       hamburger.classList.remove("is-active");
+//     }
+//   })    
