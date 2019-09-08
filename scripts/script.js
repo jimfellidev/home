4d3a9f584bc0f7ac79311da64ff4e2345b506f0f
Jim Felli <jimfellidev@gmail.com>
2019-09-08 07:15:54 -0400

    off click navdrop

--- scripts/script.js
+++ scripts/script.js
@@ -19,30 +19,30 @@ hamburger.addEventListener("click", function() {
     } 
   });
 
-//   function isDescendant(parent, child) {
-//     var node = child.parentNode;
-//     while (node != null) {
-//       if (node == parent) {
-//         return true;
-//       }
-//       node = node.parentNode;
-//     }
-//     return false;
-//   }
+  function isDescendant(parent, child) {
+    var node = child.parentNode;
+    while (node != null) {
+      if (node == parent) {
+        return true;
+      }
+      node = node.parentNode;
+    }
+    return false;
+  }
   
 // // re-write 
 
 
-//   document.addEventListener("click", function(event){
-//     console.log('event.target', event.target);    
-//     const nav = document.querySelector(".nav-wrapper");
-//     var isInNav = isDescendant(nav, event.target);
-//     console.log('isInNav', isInNav);
-//     if (hamburger.classList.contains('is-active') && isInNav) {
-//       console.log('true')      
-//     } else {
-//       console.log('false')
-//       menuId.classList.remove("show");
-//       hamburger.classList.remove("is-active");
-//     }
-//   })    
+  document.addEventListener("click", function(event){
+    console.log('event.target', event.target);    
+    const nav = document.querySelector(".nav__all");
+    var isInNav = isDescendant(nav, event.target);
+    console.log('isInNav', isInNav);
+    if (hamburger.classList.contains('is-active') && isInNav) {
+      console.log('true')      
+    } else {
+      console.log('false')
+      addDropper.classList.remove("nav__active");
+      hamburger.classList.remove("nav__plus--active");
+    }
+  })    
